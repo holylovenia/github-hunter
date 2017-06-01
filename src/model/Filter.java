bc6a36994c87511ba1ba1c3bbd45286e2efb37bd
Holy Lovenia <holy.lovenia@gmail.com>
2017-06-01 17:09:25 +0700

    Added Filter.java

---
+++ src/model/Filter.java
@@ -0,0 +1,36 @@
+package model;
+
+/**
+ * Created by Holy on 01-Jun-17.
+ */
+public class Filter {
+    private boolean used;
+    private String boundOperator;
+    private int boundNumber;
+
+    public Filter() {}
+    public void setFilter(boolean _used) {
+        used = _used;
+    }
+    public void setFilter(boolean _used, String _boundOperator, int _boundNumber) {
+        used = _used;
+        if(isBoundOperatorValid(_boundOperator)) {
+            boundOperator = _boundOperator;
+        } else {
+            boundOperator = "<=";
+        }
+        boundNumber = _boundNumber;
+    }
+    public boolean isBoundOperatorValid(String _boundOperator) {
+        return(_boundOperator == "<" || _boundOperator == "<=" || _boundOperator == ">" || _boundOperator == ">=");
+    }
+    public boolean getUsed() {
+        return used;
+    }
+    public String getBoundOperator() {
+        return boundOperator;
+    }
+    public int getBoundNumber() {
+        return boundNumber;
+    }
+}
